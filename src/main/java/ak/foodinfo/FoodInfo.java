1ab85b5bf700111346223850c78f77d07223cf1a
aksource <akira.mew@gmail.com>
2019-04-24 00:20:44 +0900

    1.13-1.4.0
    1.13対応
    gitのディレクトリ構成変更

--- src/main/java/ak/foodinfo/FoodInfo.java
+++ src/main/java/ak/foodinfo/FoodInfo.java
@@ -1,36 +1,26 @@
 package ak.foodinfo;
 
-import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.translation.I18n;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
 import java.text.DecimalFormat;
 import java.util.List;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = FoodInfo.MOD_ID,
-        name = FoodInfo.MOD_NAME,
-        version = FoodInfo.MOD_VERSION,
-        dependencies = FoodInfo.MOD_DEPENDENCIES,
-        useMetadata = true,
-        acceptedMinecraftVersions = FoodInfo.MOD_MC_VERSION)
+@Mod(FoodInfo.MOD_ID)
 public class FoodInfo {
     public static final String MOD_ID = "foodinfo";
-    public static final String MOD_NAME = "FoodInfo";
-    public static final String MOD_VERSION = "@VERSION@";
-    public static final String MOD_DEPENDENCIES = "required-after:forge";
-    public static final String MOD_MC_VERSION = "[1.12,1.99.99]";
     private static final String KEY_FOOD_POINT = "foodinfo.info.0";
     private static final String KEY_HIDDEN_FOOD_POINT = "foodinfo.info.1";
     private static final String KEY_WOLF_LIKE = "foodinfo.info.2";
     private static final DecimalFormat DECIMALFORMAT = new DecimalFormat("#.#");
 
-    @Mod.EventHandler
-    public void load(FMLInitializationEvent event) {
+    public FoodInfo() {
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -38,17 +28,18 @@ public class FoodInfo {
     public void addFoodInfo(ItemTooltipEvent event) {
         ItemStack itemStack = event.getItemStack();
         if (itemStack.getItem() instanceof ItemFood) {
-            List<String> list = event.getToolTip();
+            List<ITextComponent> list = event.getToolTip();
             int foodPoint = ((ItemFood) itemStack.getItem()).getHealAmount(itemStack);
             float foodSaturationModifier = ((ItemFood) itemStack.getItem()).getSaturationModifier(itemStack);
             float hiddenFoodPoint = foodPoint * foodSaturationModifier;
-            String string = I18n.translateToLocalFormatted(KEY_FOOD_POINT, DECIMALFORMAT.format((float) foodPoint / 2));
-            String stringHidden = I18n.translateToLocalFormatted(KEY_HIDDEN_FOOD_POINT, DECIMALFORMAT.format(hiddenFoodPoint));
-            list.add(string);
-            list.add(stringHidden);
-            if (((ItemFood) itemStack.getItem()).isWolfsFavoriteMeat()) {
-                String stringWolfLike = I18n.translateToLocal(KEY_WOLF_LIKE);
-                list.add(stringWolfLike);
+            String string = I18n
+                .format(KEY_FOOD_POINT, DECIMALFORMAT.format((float) foodPoint / 2));
+            String stringHidden = I18n.format(KEY_HIDDEN_FOOD_POINT, DECIMALFORMAT.format(hiddenFoodPoint));
+            list.add(new TextComponentString(string));
+            list.add(new TextComponentString(stringHidden));
+            if (((ItemFood) itemStack.getItem()).isMeat()) {
+                String stringWolfLike = I18n.format(KEY_WOLF_LIKE);
+                list.add(new TextComponentString(stringWolfLike));
             }
         }
     }
